b41fc58e94daa64bf1cc904cde03a618b5dd71e6
PS2006m <prathamshah485@gmail.com>
2025-03-15 20:13:35 +0530

    Third

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=wo+w9*g^h_7=6dpsy*$@gw9=6t7ow4%jdgc7%9usu-dn&%vt%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
