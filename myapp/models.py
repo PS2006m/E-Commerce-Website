ae9dbf6ee590a2bbf765c8888c8c9fa73c527f2e
PS2006m <prathamshah485@gmail.com>
2025-03-17 22:59:11 +0530

    CloudiCommit1

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 class ChatMessage(models.Model):
     username = models.CharField(max_length=255)  # Store username
     message = models.TextField()
@@ -22,7 +22,7 @@ class Courses(models.Model):
     course_name=models.CharField(max_length=100)
     course_price=models.FloatField()
     course_length=models.CharField(max_length=100)
-    course_img = models.ImageField(upload_to='media/course_images/')
+    course_img = CloudinaryField('image')
     course_bought=models.PositiveIntegerField(default=0)
     
     def __str__(self):
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'channels',
+    'cloudinary',
+    'cloudinary_storage'
 ]
 
 # ✅ ASGI application setup
@@ -130,6 +132,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dkty1thga',
+    'API_KEY': '849772912341497',
+    'API_SECRET': '0MTPHt57gbHhk77cVEE1w0zKIUc'
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# MEDIA_URL = '/media/'  # Optional
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
